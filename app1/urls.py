c22731cfb7febf03d449cc282af8510f1afdbea4
taminaj <tamaraaguayo11@gmail.com>
2026-09-10 21:55:38 -0300

    app1 lista

--- app1/urls.py
+++ app1/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 app_name = "app1"
 urlpatterns = [
-    path('vista1/', views.vista1_app1, name='vista1_app1'),
-    path('vista2/', views.vista2_app1, name='vista2_app1'),
+    path('vista1_app1/', views.vista1_app1, name='vista1_app1'),
+    path('vista2_app1/', views.vista2_app1, name='vista2_app1'),
 ]
